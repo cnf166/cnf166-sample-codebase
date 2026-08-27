6d8a6b4f272cfdabae855dbc551b7e73b2fbb921
cnf166 <nguyenvietanh166.fw@gmail.com>
2026-08-27 13:24:30 +0700

    init code base spring boot

---
+++ src/main/java/vn/cnf166/Cnf166SampleCodebaseApplication.java
@@ -0,0 +1,13 @@
+package vn.cnf166;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cnf166SampleCodebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cnf166SampleCodebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/cnf166/Cnf166SampleCodebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.cnf166;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cnf166SampleCodebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
