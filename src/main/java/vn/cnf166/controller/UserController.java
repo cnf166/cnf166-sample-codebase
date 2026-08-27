a398fd260a60d53b531226c62f355374085d0c84
cnf166 <nguyenvietanh166.fw@gmail.com>
2026-08-28 01:03:01 +0700

    return the response data with ResponseEntity

--- src/main/java/vn/cnf166/controller/UserController.java
+++ src/main/java/vn/cnf166/controller/UserController.java
@@ -2,59 +2,61 @@ package vn.cnf166.controller;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import vn.cnf166.dto.request.UserRequestDTO;
+import vn.cnf166.dto.response.ResponseSuccess;
 import vn.cnf166.util.Gender;
 import vn.cnf166.util.UserStatus;
-import vn.cnf166.util.UserType;
 
 import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-
+@Validated
 public class UserController {
 
 	@PostMapping("/add")
 	//@RequestMapping(method = RequestMethod.POST, path = "/", headers = "apiKey=v1.0")
-	public String addUser(@Valid @RequestBody UserRequestDTO userDTO) {
-		return "Added user successfully!";
+	public ResponseSuccess addUser(@Valid @RequestBody UserRequestDTO userDTO) {
+		return new ResponseSuccess(HttpStatus.CREATED, "Added user successfully!", 1);
 	}
 
 	@PutMapping("/{userId}")
-	public String updateUser(@PathVariable int userId, @RequestBody UserRequestDTO userDTO) {
+	public ResponseSuccess updateUser(@PathVariable int userId, @RequestBody UserRequestDTO userDTO) {
 		System.out.println("Update user with userid = " + userId);
-		return "Updated user successfully!";
+		return new ResponseSuccess(HttpStatus.ACCEPTED, "Updated user successfully!");
 	}
 
 	@PatchMapping("/{userId}")
-	public String changeStatusUser(@Min(1) @PathVariable int userId, @Min(1) @RequestParam(required = false) boolean status) //required = false --> non-mandatory
+	public ResponseSuccess changeStatusUser(@Min(1) @PathVariable int userId, @Min(1) @RequestParam(required = false) boolean status) //required = false --> non-mandatory
 	{
 		System.out.println("Change status user with userId =" + userId);
-		return "Change status user successfully!";
+		return new ResponseSuccess(HttpStatus.ACCEPTED, "Change status user successfully!");
 	}
 
 	@DeleteMapping("/{userId}")
-	public String deleteUser(@Min(1) @PathVariable int userId) {
+	public ResponseSuccess deleteUser(@Min(1) @PathVariable int userId) {
 		System.out.println("Delete user with userId = " + userId);
-		return "Delete user successfully";
+		return new ResponseSuccess(HttpStatus.NO_CONTENT, "Delete user successfully");
 	}
 
 	@GetMapping("/{userId}")
-	public UserRequestDTO getUser(@PathVariable int userId) {
+	public ResponseSuccess getUser(@PathVariable int userId) {
 		System.out.println("Request get user by userId: " + userId);
-		return new UserRequestDTO("Viet Anh", "Nguyen Viet Anh", "nguyenvietanh166.fw@gmail.com", "0912xxxxxx", UserStatus.NONE, Gender.FEMALE, "",  "Nam Tu Liem", new Date(), List.of("ABC", "XYZ"));
+		return new ResponseSuccess(HttpStatus.OK, "Get user by id: ", new UserRequestDTO("Viet Anh", "Nguyen Viet Anh", "nguyenvietanh166.fw@gmail.com", "0912xxxxxx", UserStatus.NONE, Gender.FEMALE, "",  "Nam Tu Liem", new Date(), List.of("ABC", "XYZ")));
 	}
 
 	@GetMapping("/users_list")
-	public List<UserRequestDTO> getAllUserList(
+	public ResponseSuccess getAllUserList(
 			@RequestParam(required = false) String phone,
 			@RequestParam(defaultValue = "1") int pageNumber,
 			@RequestParam(defaultValue = "20") int pageSize) {
 		System.out.println("Request get all users: ");
-		return List.of(new UserRequestDTO("Viet Anh", "Nguyen Viet Anh", "nguyenvietanh166.fw@gmail.com", "0912xxxxxx", UserStatus.ACTIVE, Gender.MALE, "UserType.MEMBER", "Nam Tu Liem", new Date(), List.of("ABC", "XYZ")),
-				new UserRequestDTO("Viet Anh", "Nguyen Viet Anh", "nguyenvietanh166.fw@gmail.com", "0912xxxxxx", UserStatus.NONE, Gender.OTHER, "UserType.OWNER", "Nam Tu Liem", new Date(), List.of("ABC", "XYZ")));
+		return new ResponseSuccess(HttpStatus.OK, "Get users: ", List.of(new UserRequestDTO("Viet Anh", "Nguyen Viet Anh", "nguyenvietanh166.fw@gmail.com", "0912xxxxxx", UserStatus.ACTIVE, Gender.MALE, "UserType.MEMBER", "Nam Tu Liem", new Date(), List.of("ABC", "XYZ")),
+				new UserRequestDTO("Viet Anh", "Nguyen Viet Anh", "nguyenvietanh166.fw@gmail.com", "0912xxxxxx", UserStatus.NONE, Gender.OTHER, "UserType.OWNER", "Nam Tu Liem", new Date(), List.of("ABC", "XYZ"))));
 	}
 
 }
