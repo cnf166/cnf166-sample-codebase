3e27274d15d8bcb93903beec3b424803f20d509f
cnf166 <nguyenvietanh166.fw@gmail.com>
2026-08-27 14:39:00 +0700

    add DELETE, PATCH, PUT method api endpoint

--- src/main/java/vn/cnf166/controller/UserController.java
+++ src/main/java/vn/cnf166/controller/UserController.java
@@ -10,6 +10,27 @@ public class UserController {
 
 	@PostMapping("/")
 	public String addUser(@RequestBody UserRequestDTO userDTO) {
-		return "hello" + userDTO;
+		return "Added user successfully!";
 	}
+
+	@PutMapping("/{userId}")
+	public String updateUser(@PathVariable int userId, @RequestBody UserRequestDTO userDTO) {
+		System.out.println("Update user with userid = " + userId);
+		return "Updated user successfully!";
+	}
+
+	@PatchMapping("/{userId}")
+	public String changeStatusUser(@PathVariable int userId, @RequestParam(required = false) boolean status) //required = false --> non-mandatory
+	{
+		System.out.println("Change status user with userId =" + userId);
+		return "Change status user successfully!";
+	}
+
+	@DeleteMapping("/{userId}")
+	public String deleteUser(@PathVariable int userId) {
+		System.out.println("Delete user with userId = " + userId);
+		return "Delete user successfully";
+	}
+
+
 }
